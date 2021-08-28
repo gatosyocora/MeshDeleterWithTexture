50eea42981c63726aae22ffc6dcf6ef4343dcb73
gatosyocora <gatosyocora36@gmail.com>
2021-08-28 19:23:04 +0900

    MiniMonitorのmaterialをAssetRepositoryのものに変更

--- Assets/MeshDeleterWithTexture/Editor/Views/GatoGUILayout.cs
+++ Assets/MeshDeleterWithTexture/Editor/Views/GatoGUILayout.cs
@@ -2,13 +2,14 @@
 using UnityEditor;
 using System.Linq;
 using System.IO;
+using Gatosyocora.MeshDeleterWithTexture.Models;
 
 namespace Gatosyocora.MeshDeleterWithTexture.Views
 {
     public static class GatoGUILayout
     {
         #region MiniMonitor Variable
-        public static Material gammaMat = Resources.Load<Material>("TextureEditMat");
+        public static Material gammaMat = AssetRepository.LoadTextureEditMaterial();
         #endregion
 
         public static Vector2 MiniMonitor(Texture texture, float width, float height, ref Rect rect, ref EventType type, bool isGammaCorrection)
