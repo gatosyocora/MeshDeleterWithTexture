35ee5d853c461295a1eba2b68515a869ef74c664
gatosyocora <gatosyocora36@gmail.com>
2021-08-28 19:39:51 +0900

    Resourcesフォルダ内のAssetを読み込むためのパスを新しいものに更新

--- Assets/MeshDeleterWithTexture/Editor/Models/AssetRepository.cs
+++ Assets/MeshDeleterWithTexture/Editor/Models/AssetRepository.cs
@@ -1,22 +1,27 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 
 namespace Gatosyocora.MeshDeleterWithTexture.Models
 {
     public static class AssetRepository
     {
+        private const string MESHDELETER_ASSETS_FOLDER_NAME = "MDwT";
+        private static readonly string MESHDELETER_COMPUTESHADER_FOLDER_PATH = Path.Combine(MESHDELETER_ASSETS_FOLDER_NAME, "ComputeShaders");
+        private static readonly string MESHDELETER_LANGUAGE_PACK_FOLDER_PATH = Path.Combine(MESHDELETER_ASSETS_FOLDER_NAME, "Lang");
+
         public static Material LoadTextureEditMaterial()
             => new Material(Shader.Find("Unlit/TextureEdit"));
 
         public static ComputeShader LoadDrawComputeShader()
-            => Resources.Load<ComputeShader>("colorchecker2");
+            => Resources.Load<ComputeShader>(Path.Combine(MESHDELETER_COMPUTESHADER_FOLDER_PATH, "colorchecker2"));
 
         public static ComputeShader LoadCreateUVMapComputeShader()
-            => Resources.Load<ComputeShader>("getUVMap");
+            => Resources.Load<ComputeShader>(Path.Combine(MESHDELETER_COMPUTESHADER_FOLDER_PATH, "getUVMap2"));
 
         public static LanguagePack[] LoadLanguagePacks()
             => new LanguagePack[] {
-                Resources.Load<LanguagePack>("Lang/EN"),
-                Resources.Load<LanguagePack>("Lang/JA")
+                Resources.Load<LanguagePack>(Path.Combine(MESHDELETER_LANGUAGE_PACK_FOLDER_PATH, "EN")),
+                Resources.Load<LanguagePack>(Path.Combine(MESHDELETER_LANGUAGE_PACK_FOLDER_PATH, "JA"))
             };
     }
 }
