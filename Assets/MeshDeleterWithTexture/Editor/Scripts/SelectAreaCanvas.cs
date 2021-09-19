088a1d6ef0ea81c2a26b0673e477dc346f36777e
gatosyocora <gatosyocora36@gmail.com>
2021-09-20 00:44:54 +0900

    範囲選択機能用のクラスを作成

---
+++ Assets/MeshDeleterWithTexture/Editor/Scripts/SelectAreaCanvas.cs
@@ -0,0 +1,17 @@
+﻿using Gatosyocora.MeshDeleterWithTexture.Models;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gatosyocora.MeshDeleterWithTexture
+{
+    public class SelectAreaCanvas : MonoBehaviour
+    {
+        private Material editMat;
+
+        public SelectAreaCanvas(ref Material editMat)
+        {
+            this.editMat = editMat;
+        }
+    }
+}
